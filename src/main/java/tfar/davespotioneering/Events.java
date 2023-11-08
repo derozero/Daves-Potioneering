d2baf62b47ea39c17fd0705fb14b840fc11a5676
derozero <138532129+derozero@users.noreply.github.com>
2023-11-08 23:11:47 +0000

    Update Events.java
    oh boy i hope this wont break anything!

--- src/main/java/tfar/davespotioneering/Events.java
+++ src/main/java/tfar/davespotioneering/Events.java
@@ -25,21 +25,10 @@ import net.minecraftforge.eventbus.api.Event;
 import tfar.davespotioneering.block.LayeredReinforcedCauldronBlock;
 import tfar.davespotioneering.duck.BrewingStandDuck;
 import tfar.davespotioneering.init.ModPotions;
-import tfar.davespotioneering.item.UmbrellaItem;
-import tfar.davespotioneering.menu.AdvancedBrewingStandMenu;
-import tfar.davespotioneering.mixin.BrewingStandContainerAccess;
 
 import java.util.List;
 
 public class Events {
-
-    public static void potionCooldown(PlayerInteractEvent.RightClickItem e) {
-        ItemStack stack = e.getItemStack();
-        Player player = e.getEntity();
-        if (!player.level.isClientSide && stack.getItem() instanceof ThrowablePotionItem) {
-            player.getCooldowns().addCooldown(stack.getItem(), ModConfig.Server.potion_throw_cooldown.get());
-        }
-    }
 
     public static void milkCow(PlayerInteractEvent.EntityInteractSpecific e) {
         Entity clicked = e.getTarget();
@@ -79,51 +68,10 @@ public class Events {
         }
     }
 
-    //this is called when the potion is done brewing, we use this instead of the forge event because it has a reference
-    // to the blockentity that created the potions
-    public static void potionBrew(BlockEntity brewingStandTileEntity, ItemStack ingredient) {
-        ((BrewingStandDuck)brewingStandTileEntity).addXp(Util.getBrewXp(ingredient));
-    }
-
-    public static void heldItemChangeEvent(Player player) {
-        ItemStack stack = player.getMainHandItem();
-        if ((stack.getItem() instanceof LingeringPotionItem || stack.getItem() instanceof SplashPotionItem)) {
-            player.getCooldowns().addCooldown(Items.SPLASH_POTION, ModConfig.Server.potion_switch_cooldown.get());
-            player.getCooldowns().addCooldown(Items.LINGERING_POTION, ModConfig.Server.potion_switch_cooldown.get());
-        }
-    }
-
     public static void onEat(Player player, ItemStack stack) {
         List<MobEffectInstance> mobEffectInstances = PotionUtils.getMobEffects(stack);
         for (MobEffectInstance effectInstance : mobEffectInstances) {
             player.addEffect(new MobEffectInstance(effectInstance.getEffect(), Math.max(effectInstance.getDuration() / 8, 1), effectInstance.getAmplifier(), effectInstance.isAmbient(), effectInstance.showIcon()));
-        }
-    }
-
-    //this is called when the player takes a potion from the brewing stand
-    public static void playerBrew(PlayerBrewedPotionEvent e) {
-        Player player = e.getEntity();
-        if (!player.level.isClientSide) {
-            AbstractContainerMenu container = player.containerMenu;
-            BlockEntity entity = null;
-            if (container instanceof BrewingStandMenu) {
-                entity = (BrewingStandBlockEntity)((BrewingStandContainerAccess)container).getBrewingStand();
-            } else if (container instanceof AdvancedBrewingStandMenu advancedBrewingStandMenu) {
-                entity = advancedBrewingStandMenu.blockEntity;
-            }
-
-            if (entity != null) {
-                ((BrewingStandDuck)entity).dump(player);
-            }
-        }
-    }
-
-    public static void canApplyEffect(MobEffectEvent.Applicable e) {
-        LivingEntity entity = e.getEntity();
-        if (entity instanceof Player player) {
-            if (player.getUseItem().getItem() instanceof UmbrellaItem) {
-                e.setResult(Event.Result.DENY);
-            }
         }
     }
 
@@ -148,7 +96,6 @@ public class Events {
     }
 
     public static void register() {
-        MinecraftForge.EVENT_BUS.addListener(Events::potionCooldown);
         MinecraftForge.EVENT_BUS.addListener(Events::milkCow);
         MinecraftForge.EVENT_BUS.addListener(Events::afterHit);
 
