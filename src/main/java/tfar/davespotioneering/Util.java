9b7e202c3d2d83c873b01fb5722a1e68f168c7e8
derozero <138532129+derozero@users.noreply.github.com>
2023-11-08 23:13:53 +0000

    Update Util.java
    man i dont know

--- src/main/java/tfar/davespotioneering/Util.java
+++ src/main/java/tfar/davespotioneering/Util.java
@@ -37,25 +37,6 @@ public class Util {
         ((ItemAccess) item).setMaxStackSize(count);
     }
 
-    //brewing xp is determined by the ingredient used, more valuable ingredients should give more xp
-    public static double getBrewXp(ItemStack stack) {
-        return stack.getRarity() == Rarity.RARE ? 10 : 7;
-    }
-
-    public static void splitAndSpawnExperience(Level world, Vec3 pos, double experience) {
-        world.addFreshEntity(new ExperienceOrb(world, pos.x, pos.y, pos.z, (int) experience));
-    }
-
-    public static void brewPotions(NonNullList<ItemStack> inputs, ItemStack ingredient, int[] inputIndexes) {
-        for (int i : inputIndexes) {
-            ItemStack output = BrewingRecipeRegistry.getOutput(inputs.get(i), ingredient);
-            output.setCount(inputs.get(i).getCount());//the change from the forge version
-            if (!output.isEmpty()) {
-                inputs.set(i, output);
-            }
-        }
-    }
-
     public static boolean isValidInputCountInsensitive(ItemStack stack) {
         for (IBrewingRecipe recipe : BrewingRecipeRegistry.getRecipes()) {
             if (recipe.isInput(stack)) {
