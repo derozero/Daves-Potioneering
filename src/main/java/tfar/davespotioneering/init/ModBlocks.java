6dd3edeab665adf1f3259549bf4a24566047c163
derozero <138532129+derozero@users.noreply.github.com>
2023-11-08 23:23:32 +0000

    Update ModBlocks.java

--- src/main/java/tfar/davespotioneering/init/ModBlocks.java
+++ src/main/java/tfar/davespotioneering/init/ModBlocks.java
@@ -14,16 +14,9 @@ public class ModBlocks {
 
     private static List<Block> MOD_BLOCKS;
 
-    public static final Block COMPOUND_BREWING_STAND = new AdvancedBrewingStandBlock(BlockBehaviour.Properties.of(Material.METAL)
-            .requiresCorrectToolForDrops().strength(0.5F).lightLevel(state -> 1).noOcclusion());
-
     public static final Block REINFORCED_CAULDRON = new ReinforcedCauldronBlock(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.STONE)
             .requiresCorrectToolForDrops().strength(0.5F).lightLevel(state -> 1).noOcclusion(),ModCauldronInteractions.EMPTY);
 
     public static final Block REINFORCED_WATER_CAULDRON = new LayeredReinforcedCauldronBlock(BlockBehaviour.Properties.copy(REINFORCED_CAULDRON));
-
-    public static final Block MAGIC_LECTERN = new LecternBlock(BlockBehaviour.Properties.copy(Blocks.LECTERN));
-
-    public static final Block POTION_INJECTOR = new PotionInjectorBlock(BlockBehaviour.Properties.copy(Blocks.FLETCHING_TABLE).noOcclusion());
 
 }
