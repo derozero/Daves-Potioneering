0a6d8b28c5df43dce2c20d30ffb92501a4b38bbc
derozero <138532129+derozero@users.noreply.github.com>
2023-11-11 17:53:07 +0000

    Add files via upload
    i dont know what im doing and i hope adding these causes mc to stop crashing

---
+++ src/main/java/tfar/davespotioneering/mixin/BrewingStandBlockEntityMixin.java
@@ -0,0 +1,48 @@
+package tfar.davespotioneering.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.BrewingStandBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import tfar.davespotioneering.Events;
+import tfar.davespotioneering.Util;
+import tfar.davespotioneering.duck.BrewingStandDuck;
+
+@Mixin(BrewingStandBlockEntity.class)
+public class BrewingStandBlockEntityMixin extends BlockEntity implements BrewingStandDuck {
+
+    protected double xp;
+
+    public BrewingStandBlockEntityMixin(BlockEntityType<?> p_155228_, BlockPos p_155229_, BlockState p_155230_) {
+        super(p_155228_, p_155229_, p_155230_);
+    }
+
+
+    @Override
+    public void addXp(double xp) {
+        this.xp += xp;
+    }
+
+    @Override
+    public void dump(Player player) {
+        if (xp > 0) {
+            xp = 0;
+            Util.splitAndSpawnExperience(level, player.position(), xp);
+            this.setChanged();
+        }
+    }
+
+    @Inject(method = "doBrew",at = @At(value = "INVOKE",target = "Lnet/minecraftforge/event/ForgeEventFactory;onPotionBrewed(Lnet/minecraft/core/NonNullList;)V",remap = false))
+    private static void betterIntercept(Level itemstack1, BlockPos p_155291_, NonNullList<ItemStack> p_155292_, CallbackInfo ci) {
+        Events.potionBrew(itemstack1.getBlockEntity(p_155291_),p_155292_.get(3));
+    }
+}
