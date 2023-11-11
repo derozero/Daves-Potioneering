6e94668559a175b54a287ea193e1a01cfc0435f4
derozero <138532129+derozero@users.noreply.github.com>
2023-11-11 17:54:10 +0000

    Update BrewingStandBlockEntityMixin.java

--- src/main/java/tfar/davespotioneering/mixin/BrewingStandBlockEntityMixin.java
+++ src/main/java/tfar/davespotioneering/mixin/BrewingStandBlockEntityMixin.java
@@ -20,29 +20,5 @@ import tfar.davespotioneering.duck.BrewingStandDuck;
 @Mixin(BrewingStandBlockEntity.class)
 public class BrewingStandBlockEntityMixin extends BlockEntity implements BrewingStandDuck {
 
-    protected double xp;
-
-    public BrewingStandBlockEntityMixin(BlockEntityType<?> p_155228_, BlockPos p_155229_, BlockState p_155230_) {
-        super(p_155228_, p_155229_, p_155230_);
-    }
-
-
-    @Override
-    public void addXp(double xp) {
-        this.xp += xp;
-    }
-
-    @Override
-    public void dump(Player player) {
-        if (xp > 0) {
-            xp = 0;
-            Util.splitAndSpawnExperience(level, player.position(), xp);
-            this.setChanged();
-        }
-    }
-
-    @Inject(method = "doBrew",at = @At(value = "INVOKE",target = "Lnet/minecraftforge/event/ForgeEventFactory;onPotionBrewed(Lnet/minecraft/core/NonNullList;)V",remap = false))
-    private static void betterIntercept(Level itemstack1, BlockPos p_155291_, NonNullList<ItemStack> p_155292_, CallbackInfo ci) {
-        Events.potionBrew(itemstack1.getBlockEntity(p_155291_),p_155292_.get(3));
     }
 }
